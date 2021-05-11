5832098c5b80510b6ae1b311fdc761396f60a077
radusori <radu.lamoran@gmail.com>
2021-05-11 18:58:27 +0300

    testgit

--- BasketballTinder/Form1.cs
+++ BasketballTinder/Form1.cs
@@ -16,5 +16,10 @@ namespace BasketballTinder
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
